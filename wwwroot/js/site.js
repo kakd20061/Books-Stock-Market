3d6d6b6e2e18d155e15800f09b56683fc5cdcbdf
Kajetan Dąbrowski <gizard.biznes123@vp.pl>
2023-01-14 13:05:52 +0100

    marketplace filters

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -19,24 +19,85 @@
     }
 }
 
-function setId(id) {
+function setId(id)
+{
     const idInput = document.getElementById("annId");
     idInput.value = id;
 }
 
-function setIdRespond(id,isOffer) {
+function setIdRespond(id, isOffer)
+{
     const idInput = document.getElementById("announId");
     idInput.value = id;
     const bool = document.getElementById("isoffer");
     bool.value = isOffer;
 }
 
-function setIdRespondToRespond(id) {
+function setIdRespondToRespond(id)
+{
     const idInput = document.getElementById("respId");
     idInput.value = id;
 }
 
-function setOffId(id) {
+function setOffId(id)
+{
     const idInput = document.getElementById("offId");
     idInput.value = id;
+}
+
+function filters()
+{
+    const radio = document.querySelector('input[name="grade"]:checked').value;
+    const cards = document.getElementsByClassName("cardHid");
+
+    let array = []
+    let checkboxes = document.querySelectorAll('input[name=subject]:checked')
+
+    for (var i = 0; i < checkboxes.length; i++) {
+        array.push(checkboxes[i].value)
+    }
+
+    if (radio == "all") {
+        for (let i = 0; i < cards.length; i++) {
+            if (array.length > 0) {
+                for (let j = 0; j < array.length; j++) {
+                    if (cards[i].classList.contains("subject-" + array[j])) {
+                        cards[i].style.display = "block";
+                        break;
+                    }
+                    else {
+                        cards[i].style.display = "none";
+                    }
+                }
+            }
+            else {
+                cards[i].style.display = "block";
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < cards.length; i++) {
+            if (cards[i].classList.contains("grade-" + radio)) {
+                if (array.length > 0) {
+                    for (let j = 0; j < array.length; j++) {
+                        if (cards[i].classList.contains("subject-" + array[j])) {
+                            cards[i].style.display = "block";
+                            break;
+                        }
+                        else {
+                            cards[i].style.display = "none";
+                        }
+                    }
+                }
+                else {
+                    cards[i].style.display = "block";
+                }
+            }
+            else {
+                cards[i].style.display = "none";
+            }
+        }
+    }
+
+    return false;
 }
