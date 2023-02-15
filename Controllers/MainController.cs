a4fbc21c3b0ed812047fe216e148fcc5c27bd6ec
kakd20061 <gizard.biznes123@vp.pl>
2023-02-15 16:36:52 +0100

    register summary structure

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -18,6 +18,11 @@ namespace Books_Stock_Market.Controllers
             return View();
         }
 
+        public IActionResult RegisterSummary()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             return View();
